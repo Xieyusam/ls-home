1039c9c15305c3d62fcda65806cf5260cbee4e54
xieyushen <yushen.xie@xjmz.com>
2024-06-25 11:05:51 +0800

    first commit

---
+++ js/projectInfo.js
@@ -0,0 +1,86 @@
+const project1 = {
+  title: "《Hearing the bullseye》",
+  time: "2021-2022",
+  anthor: "Shan Luo*, Jianan Liu, Botao Hu.",
+  award: [
+    {
+      time: "June 15 2021",
+      desc: "How can we facilitate VI individuals to play together with sighted individuals? In the VI community, social play plays a crucial role as it.",
+      link: "",
+    },
+    {
+      time: "June 15 2021",
+      desc: "How can we facilitate VI individuals to play together with sighted individuals? In the VI community, social play plays a crucial role as it.",
+      link: "",
+    },
+    {
+      time: "June 15 2021",
+      desc: "How can we facilitate VI individuals to play together with sighted individuals? In the VI community, social play plays a crucial role as it.",
+      link: "",
+    },
+  ],
+  contant: [
+    {
+      type: "img",
+      text: "./image/project/project-1.png",
+    },
+    {
+      type: "text",
+      text: `I graduated with a master's degree from the China Academy of Art, majoring in Accessible Design. I have served as a product manager at an AI company for half a year and am currently a researcher in the field of accessibility at the China Academy of Art. My research direction is to explore the imbalances and fairness that exist between blind and sighted individuals in different relationships, and to design systematic solutions through creative and fun game interactions for daily communication scenarios between the blind and the sighted. My research strengths lie in the innovative design thinking that integrates art and technology, achieving humanistic creative solutions through intelligent systems. I hope that design can use technology to build more bridges between people with disabilities and society. I am applying for a PhD in human-computer interaction and an academic assistantship.`,
+    },
+    {
+        type: "img",
+        text: "./image/project/project-1.png",
+      },
+      {
+        type: "text",
+        text: `I graduated with a master's degree from the China Academy of Art, majoring in Accessible Design. I have served as a product manager at an AI company for half a year and am currently a researcher in the field of accessibility at the China Academy of Art. My research direction is to explore the imbalances and fairness that exist between blind and sighted individuals in different relationships, and to design systematic solutions through creative and fun game interactions for daily communication scenarios between the blind and the sighted. My research strengths lie in the innovative design thinking that integrates art and technology, achieving humanistic creative solutions through intelligent systems. I hope that design can use technology to build more bridges between people with disabilities and society. I am applying for a PhD in human-computer interaction and an academic assistantship.`,
+      }, 
+  ],
+};
+
+const project2 = {
+    title: "《Hearing the bullseye2》",
+    time: "2021-2022",
+    anthor: "Shan Luo*, Jianan Liu, Botao Hu.",
+    award: [
+      {
+        time: "June 15 2021",
+        desc: "How can we facilitate VI individuals to play together with sighted individuals? In the VI community, social play plays a crucial role as it.",
+        link: "",
+      },
+      {
+        time: "June 15 2021",
+        desc: "How can we facilitate VI individuals to play together with sighted individuals? In the VI community, social play plays a crucial role as it.",
+        link: "",
+      },
+      {
+        time: "June 15 2021",
+        desc: "How can we facilitate VI individuals to play together with sighted individuals? In the VI community, social play plays a crucial role as it.",
+        link: "",
+      },
+    ],
+    contant: [
+      {
+        type: "img",
+        text: "./image/detail/detail-1.png",
+      },
+      {
+        type: "text",
+        text: `I graduated with a master's degree from the China Academy of Art, majoring in Accessible Design. I have served as a product manager at an AI company for half a year and am currently a researcher in the field of accessibility at the China Academy of Art. My research direction is to explore the imbalances and fairness that exist between blind and sighted individuals in different relationships, and to design systematic solutions through creative and fun game interactions for daily communication scenarios between the blind and the sighted. My research strengths lie in the innovative design thinking that integrates art and technology, achieving humanistic creative solutions through intelligent systems. I hope that design can use technology to build more bridges between people with disabilities and society. I am applying for a PhD in human-computer interaction and an academic assistantship.`,
+      },
+      {
+          type: "img",
+          text: "./image/detail/detail-1.png",
+        },
+        {
+          type: "text",
+          text: `I graduated with a master's degree from the China Academy of Art, majoring in Accessible Design. I have served as a product manager at an AI company for half a year and am currently a researcher in the field of accessibility at the China Academy of Art. My research direction is to explore the imbalances and fairness that exist between blind and sighted individuals in different relationships, and to design systematic solutions through creative and fun game interactions for daily communication scenarios between the blind and the sighted. My research strengths lie in the innovative design thinking that integrates art and technology, achieving humanistic creative solutions through intelligent systems. I hope that design can use technology to build more bridges between people with disabilities and society. I am applying for a PhD in human-computer interaction and an academic assistantship.`,
+        }, 
+    ],
+  };
+
+const projectInfoData = {
+  project1,
+  project2
+};
